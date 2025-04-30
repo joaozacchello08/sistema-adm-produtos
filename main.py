86c9dad704d7989cfb0c07d38570d372d6bd229f
Joao Carlos <xonguinhos@gmail.com>
2025-04-30 19:27:50 -0300

    db completed

--- main.py
+++ main.py
@@ -1,8 +1,3 @@
 from src.adm_db import start_db, insert_new_product, remove_product, update_product, get_product
 
 start_db()
-
-# test suit
-if __name__ == "__main__":
-    insert_new_product("a", 2.99, 3, "12345678", "/")
-    print(get_product(1))
